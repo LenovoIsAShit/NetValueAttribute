3ee8e4828e643237b998d2a6f28b12b9ffa87db4
MatherFacker <2352272505@qq.com>
2025-08-10 19:24:33 +0800

    [反射工具] done

---
+++ NetSyncObject/NetSyncObject.cs
@@ -0,0 +1,556 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Net;
+
+
+namespace NetSyncObject
+{
+    ///////////////////////////////  客机 开始 ///////////////////////////////
+    public abstract class SyncClientObject: IClientBehavior
+    {
+        //默认数据集合
+        BasicClientData basicClientData;
+        //反射同步数据集合
+        CustomClientData customClientData;
+        //客户端状态枚举
+        public enum ClientState
+        {
+            Send,Received,Start,Close,Broadcast
+        }
+
+        public SyncClientObject()
+        {
+            basicClientData = new BasicClientData();
+            customClientData = new CustomClientData();
+        }
+
+
+        public void OnAction(ClientState cmd)
+        {
+            switch (cmd)
+            {
+                case ClientState.Send:
+                    SendData();
+                    break;
+                case ClientState.Received:
+                    ReceiveData();
+                    break;
+                case ClientState.Start:
+                    StartClient();
+                    break;
+                case ClientState.Close:
+                    CloseClient();
+                    break;
+                case ClientState.Broadcast:
+                    break;
+            }
+        }
+
+
+        public void StartClient()
+        {
+            
+        }
+
+        public void CloseClient()
+        {
+
+        }
+
+        public void SendData()
+        {
+
+        }
+
+        public void ReceiveData()
+        {
+
+        }
+
+        public void Broadcast()
+        {
+            //序列化询问消息
+            QAData quest = new QAData(
+                QAData.QuestState.QueryHost, 
+                basicClientData.clientIP, 
+                basicClientData.ReceivePort
+            );
+            string jsonData = JsonSerializer.Serialize(quest);
+            byte[] datas = Encoding.UTF8.GetBytes(jsonData);
+            int sendPort = basicClientData.SendPort;
+
+            //局域网广播
+            UdpClient sender = new UdpClient();
+            sender.EnableBroadcast = true;
+            sender.SendAsync(datas, datas.Length, new IPEndPoint(IPAddress.Broadcast, sendPort));
+        }
+    }
+
+
+    /// <summary>
+    /// 客户端行为约束
+    /// </summary>
+    interface IClientBehavior
+    {
+        //开启客户端
+        void StartClient();
+        //关闭客户端
+        void CloseClient();
+        //发送消息
+        void SendData();
+        //接收消息后回调
+        void ReceiveData();
+        //广播
+        void Broadcast();
+    }
+
+
+
+    /// <summary>
+    /// 属性集合基类
+    /// </summary>
+    public abstract class BaseDataSet
+    {
+        public List<Object> GetAllReflectData()
+        {
+            return SyncReflectionTool.Instance.Get(GetType(), this);
+        }
+
+        public void SetAllReflectData(List<Object> Values)
+        {
+            SyncReflectionTool.Instance.Set(GetType(), this, Values);
+        }
+    }
+
+
+    /// <summary>
+    /// 默认数据集合
+    /// </summary>
+    internal class BasicClientData : BaseDataSet
+    {
+        //本机IP
+        public string clientIP;
+        //主机IP
+        public string HostIP;
+        //发送端口
+        public int SendPort;
+        //接收端口
+        public int ReceivePort;
+        //客户端发送套接字
+        public UdpClient sender;
+        //客户端接收套接字
+        public UdpClient receiver;
+        
+    }
+
+
+    /// <summary>
+    /// 自定义数据集合，可使用NetValueAttribute标识需要反射的字段
+    /// </summary>
+    public class CustomClientData : BaseDataSet
+    {
+
+    }
+
+    ///////////////////////////////  客机 结束 ///////////////////////////////
+
+
+
+
+
+
+
+    ///////////////////////////////  主机 开始 ///////////////////////////////
+    public class SyncServer
+    {
+
+    }
+
+    ///////////////////////////////  主机 结束 ///////////////////////////////
+
+
+
+
+
+
+
+    ///////////////////////////////  网络消息 开始 ///////////////////////////////
+
+    /// <summary>
+    /// 消息基类
+    /// </summary>
+    [Serializable]
+    public abstract class NetData
+    {
+        //消息请求类型
+        public enum QuestState
+        {
+            QueryHost,HostAnswer,Close,NormalData
+        }
+        public QuestState state;
+
+        //以键值对(Json)方式储存
+        public Dictionary<int, List<Object>> args;
+    }
+
+    /// <summary>
+    /// 请求消息
+    /// </summary>
+    [Serializable]
+    public class QAData : NetData
+    {
+        // 请求客机IP/回应主机IP
+        string clientIP;
+        // 请求接收端口/回应主机端口
+        int Port;
+
+        public QAData(QuestState state, string ip, int port)
+        {
+            this.state = state;
+            this.clientIP = ip;
+            this.Port = port;
+        }
+    }
+
+
+
+    ///////////////////////////////  网络消息 结束 ///////////////////////////////
+
+
+
+
+
+
+
+
+    ///////////////////////////////  反射工具 开始 ///////////////////////////////
+    internal class SyncReflectionTool
+    {
+        /// <summary>
+        /// 单例，仅此程序集可访问
+        /// </summary>
+        private static SyncReflectionTool instance;
+        public static SyncReflectionTool Instance
+        {
+            get
+            {
+                lock (instance)
+                {
+                    if (instance == null)
+                    {
+                        lock (instance){
+                            Init();
+                        }
+                    }
+                    return instance;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// 记录反射信息
+        /// </summary>
+        private static Dictionary<Type, List<FieldInfo>> _typeDic;
+
+
+        /// <summary>
+        /// 赋值和设值方式
+        /// </summary>
+        private static Dictionary<Tuple<Type, Type>, Delegate> _GetWay;
+        private static Dictionary<Tuple<Type, Type>, Delegate> _SetWay;
+
+
+        /// <summary>
+        /// 赋值或设值枚举
+        /// </summary>
+        enum GetOrSet{SetWay,GetWay}
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        private static void Init()
+        {
+            instance = new SyncReflectionTool();
+            _GetWay = new Dictionary<Tuple<Type, Type>, Delegate>();
+            _SetWay = new Dictionary<Tuple<Type, Type>, Delegate>();
+            _typeDic = new Dictionary<Type, List<FieldInfo>>();
+        }
+
+
+        /// <summary>
+        /// 反射类型，缓存下来信息
+        /// </summary>
+        /// <param name="Client"></param>
+        public void Reflection(Type Client)
+        {
+            if (_typeDic.ContainsKey(Client))
+            {
+                return;
+            }
+
+            //反射查找字段
+            BindingFlags bf = BindingFlags.Public| BindingFlags.Instance | BindingFlags.Static;
+            var filedList = Client.GetFields(bf)
+                .Where(filed=>Attribute.IsDefined(filed,typeof(NetValueAttribute))).ToList();
+
+            //缓存反射信息
+            List<FieldInfo> list = new List<FieldInfo>();
+            foreach (var filedinfo in filedList)
+            {
+                //基于反射信息缓存委托
+                AddAction(Client, filedinfo, GetOrSet.GetWay);
+                AddAction(Client, filedinfo, GetOrSet.SetWay);
+                list.Add(filedinfo);
+            }
+
+            _typeDic.Add(Client, list);
+        }
+
+
+
+        /// <summary>
+        /// 设值与赋值
+        /// </summary>
+        /// <param name="ClientType"></param>
+        /// <param name="client"></param>
+        /// <param name="Values"></param>
+        public void Set(Type ClientType, Object client, List<Object> Values)
+        {
+            if (!_typeDic.ContainsKey(ClientType)||!_typeDic.ContainsKey(ClientType))
+            {
+                return;
+            }
+
+            //获取反射信息
+            List<FieldInfo> list = _typeDic[ClientType];
+            int indx = 0;
+
+            //这里Values的值顺序和list必须相同
+            foreach (var field in list) {
+                var setter = (Action<Object,Object>)GetActionByCMD(ClientType, field.FieldType, GetOrSet.SetWay);
+                if (setter != null)
+                {
+                    setter(client, Values[indx]);
+                }
+                indx++;
+            }
+        }
+        public List<Object> Get(Type ClientType, Object client)
+        {
+            if (!_typeDic.ContainsKey(ClientType))
+            {
+                return null;
+            }
+
+            List<FieldInfo> list = _typeDic[ClientType];
+            List<Object> values = new List<Object>();
+            int indx = 0;
+            foreach (var field in list)
+            {
+                var getter = (Func<Object,Object>)GetActionByCMD(ClientType, field.FieldType, GetOrSet.GetWay);
+                if (getter != null) {
+                    var value = getter(client);
+                    if (value != null)values.Add(value);
+                }
+                indx++;
+            }
+
+            return values;
+        }
+
+
+
+        /// <summary>
+        /// 获取委托
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="value"></param>
+        /// <param name="cmd"></param>
+        /// <returns></returns>
+        private Delegate GetActionByCMD(Type client, Type value, GetOrSet cmd)
+        {
+            Tuple<Type,Type>pair=Tuple.Create(client,value);
+            Dictionary<Tuple<Type, Type>, Delegate> _Way = null;
+
+            switch (cmd)
+            {
+                case GetOrSet.GetWay:
+                    _Way = _GetWay;
+                    break;
+                case GetOrSet.SetWay:
+                    _Way = _SetWay;
+                    break;
+            }
+
+            if (_Way.ContainsKey(pair))
+            {
+                return _Way[pair];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// 缓存委托
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="value"></param>
+        /// <param name="cmd"></param>
+        private void AddAction(Type client, FieldInfo value, GetOrSet cmd)
+        {
+            Tuple<Type, Type> pair = Tuple.Create(client, value.FieldType);
+            //参数确定
+            Dictionary<Tuple<Type, Type>, Delegate> _Way = null;
+            MethodInfo method = null;
+
+            switch (cmd)
+            {
+                case GetOrSet.SetWay:
+                    _Way = _SetWay;
+                    method = CreateSetterMethodInfo(client, value);
+                    break;
+                case GetOrSet.GetWay:
+                    _Way = _GetWay;
+                    method = CreateGetterMethodInfo(client, value);
+                    break;
+            }
+
+            //缓存委托
+            if (!_Way.ContainsKey(pair))
+            {
+                var act = GetAction(client, value, method, cmd);
+                _Way.Add(pair, act);
+            }
+        }
+
+
+        /// <summary>
+        /// 基于IL动态代码，获取赋值设值动态方法
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        private MethodInfo CreateSetterMethodInfo(Type client,FieldInfo value) {
+            var dynamicMethod = new DynamicMethod(
+                name: $"Setter_{client.DeclaringType.Name}_{value.FieldType.Name}",
+                returnType: null,
+                parameterTypes: new Type[2] { typeof(Object), typeof(Object) },
+                restrictedSkipVisibility: true
+            );
+
+            //IL生成器
+            var il = dynamicMethod.GetILGenerator();
+
+            //加载两个参数
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ldarg_1);
+
+            //赋值
+            il.Emit(OpCodes.Stfld,value);
+
+            //返回
+            il.Emit(OpCodes.Ret);
+
+            return dynamicMethod;
+        }
+        private MethodInfo CreateGetterMethodInfo(Type client, FieldInfo value)
+        {
+            var dynamicMethod = new DynamicMethod(
+                name: $"Setter_{client.DeclaringType.Name}_{value.FieldType.Name}",
+                returnType: typeof(Object),
+                parameterTypes: new Type[2] { typeof(Object), typeof(Object) },
+                restrictedSkipVisibility: true
+            );
+
+            //IL生成器
+            var il = dynamicMethod.GetILGenerator();
+
+            //加载参数
+            il.Emit(OpCodes.Ldarg_0);
+
+            //取值
+            il.Emit(OpCodes.Ldfld, value);
+
+            //返回
+            il.Emit(OpCodes.Ret);
+
+            return dynamicMethod;
+        }
+
+
+        /// <summary>
+        /// 获取委托
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="value"></param>
+        /// <param name="method"></param>
+        /// <returns></returns>
+        private Delegate GetAction(Type client, FieldInfo value, MethodInfo method, GetOrSet cmd)
+        {
+            Delegate del = null;
+            switch (cmd) {
+                case GetOrSet.SetWay:
+                    del = (Action<Object, Object>)Delegate.CreateDelegate(
+                        typeof(Action<Object, Object>),
+                        null,
+                        method
+                    );
+                    break;
+                case GetOrSet.GetWay:
+                    del = (Func<Object, Object>)Delegate.CreateDelegate(
+                        typeof(Func<Object, Object>),
+                        null,
+                        method
+                    );
+                    break;
+            }
+            return del;
+        }
+    }
+
+
+    ///////////////////////////////  网络消息 结束 ///////////////////////////////
+
+
+
+
+
+
+
+
+    ///////////////////////////////  自定义Attribute 开始 ///////////////////////////////
+
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    internal class NetValueAttribute : Attribute
+    {
+        /// <summary>
+        /// 特性描述
+        /// </summary>
+        public string Desc;
+
+        public NetValueAttribute(string desc) {
+            Desc = desc;
+        }
+    }
+
+
+    ///////////////////////////////  自定义Attribute 结束 ///////////////////////////////
+}
